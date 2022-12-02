c96e8bbdedd5bd0feb259ced2a3c7e65a399ee88
Дмитрий Кирилин <106656570+dimayych02@users.noreply.github.com>
2022-12-02 18:07:16 +0400

    Update Yandex.java

--- src/main/java/Testing/Yandex.java
+++ src/main/java/Testing/Yandex.java
@@ -1,4 +1,3 @@
-package Testing;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
@@ -8,6 +7,7 @@ import org.testng.Assert;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Yandex {
@@ -29,6 +29,8 @@ public class Yandex {
         dr.get(google);
 
     }
+
+
     public void Laptops(String searching,String aHref,String S,String finding) throws InterruptedException {
         try {
             WebElement elo = dr.findElement(By.xpath(searching));
@@ -44,12 +46,16 @@ public class Yandex {
 
         }
         catch (Exception e) {
+
             WebElement bot = dr.findElement(By.xpath("//input[@type=\"submit\"]"));
             bot.click();
+            Thread.sleep(13000);
             WebElement el1 = dr.findElement(By.xpath(finding));
             el1.click();
             Thread.sleep(3000);
         }
+
+
 
     }
     public void filter(String laptops,String BrandLenovo,
@@ -68,39 +74,133 @@ public class Yandex {
 
         }
         catch (Exception e){
-                WebElement bot = dr.findElement(By.xpath("//input[@type=\"submit\"]"));
-                bot.click();
 
-                WebElement Brand = dr.findElement(By.xpath(BrandLenovo));
-                Brand.click();
+            WebElement bot = dr.findElement(By.xpath("//input[@type=\"submit\"]"));
+            bot.click();
+            Thread.sleep(13000);
+            WebElement Brand = dr.findElement(By.xpath(BrandLenovo));
+            Brand.click();
 
-                List<WebElement> pricesRange = dr.findElements(By.xpath(range));
-                pricesRange.get(0).sendKeys(min);
-                pricesRange.get(1).sendKeys(max);
-                Thread.sleep(4000);
+            List<WebElement> pricesRange = dr.findElements(By.xpath(range));
+            pricesRange.get(0).sendKeys(min);
+            pricesRange.get(1).sendKeys(max);
+            Thread.sleep(4000);
 
         }
 
-    }
-        public void ValidElements(String listOfElements,String valPrice,String minvalue,String maxvalue){
-            JavascriptExecutor js = (JavascriptExecutor) dr;
-            js.executeScript("window.scrollBy(0,13000)");
 
-            List <WebElement> LenovoLaptops = dr.findElements(By.xpath(listOfElements)).stream().collect(Collectors.toList());
-            List <WebElement> validPrice = dr.findElements(By.xpath(valPrice)).stream().collect(Collectors.toList());
-            for(WebElement element:LenovoLaptops){
+
+    }
+    public void ValidElements(String listOfElements,String valPrice,String minvalue,String maxvalue) throws InterruptedException {
+        JavascriptExecutor js = (JavascriptExecutor) dr;
+
+        js.executeScript("window.scrollBy(0,13100)");
+
+        dr.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+        List <WebElement> LenovoLaptops = dr.findElements(By.xpath(listOfElements)).stream().collect(Collectors.toList());
+        List <WebElement> validPrice = dr.findElements(By.xpath(valPrice)).stream().collect(Collectors.toList());
+        System.out.println(LenovoLaptops.size());
+        System.out.println(validPrice.size());
+        for(WebElement element:LenovoLaptops){
+
             String result = element.getAttribute("title");
+            System.out.println(result);
 
             Assert.assertTrue(result.toLowerCase().contains("lenovo"));
-            }
-            for(WebElement price:validPrice){
+
+            Assert.assertTrue(result.length()>0);
+
+
+        }
+        for(WebElement price:validPrice){
             String result1 =  price.getText().replaceAll("\\s+","").replaceAll("₽","");
-            Assert.assertTrue(Integer.parseInt(result1)>Integer.parseInt(minvalue)&&Integer.parseInt(result1)<Integer.parseInt(maxvalue));
+            System.out.println(result1);
+
+
+            Assert.assertTrue(Integer.parseInt(result1)>=Integer.parseInt(minvalue)&&Integer.parseInt(result1)<=Integer.parseInt(maxvalue));
+
+             Assert.assertTrue(result1.length()>0);
+        }
+
+
+    }
+    public void Payment(String linksOfElements){
+    JavascriptExecutor js = (JavascriptExecutor) dr;
+    js.executeScript("window.scrollBy(0,-12830)");
+    List <WebElement> LenovoLaptop = dr.findElements(By.xpath(linksOfElements));
+
+    WebElement OurLaptop =LenovoLaptop.get(0);
+    OurLaptop.click();
+    }
+
+
+
+    public void AddGood() throws InterruptedException {
+        try{
+            String window1 = dr.getWindowHandle();
+
+            Set<String> currentWindows = dr.getWindowHandles();
+            String window2 = null;
+
+            for(String window :currentWindows){
+                if(!window.equals(window1)){
+                    window2 = window;
+                    break;
+                }
+            }
+            dr.switchTo().window(window2);
+            dr.getCurrentUrl();
+        }
+        catch (Exception e){
+            WebElement bot = dr.findElement(By.xpath("//input[@type=\"submit\"]"));
+            bot.click();
+            Thread.sleep(13000);
+
+            String window1 = dr.getWindowHandle();
+
+            Set<String> currentWindows = dr.getWindowHandles();
+            String window2 = null;
+
+            for(String window :currentWindows){
+                if(!window.equals(window1)){
+                    window2 = window;
+                    break;
+                }
+            }
+            dr.switchTo().window(window2);
+            dr.getCurrentUrl();
 
         }
 
+
+
     }
+    public void SelectElement(String Good) throws InterruptedException{
+        try {
+
+            List <WebElement> buying = dr.findElements(By.xpath(Good));
+
+            WebElement buy = buying.get(0);
+            buy.click();
+        }
+
+
+        catch (Exception e){
+
+            WebElement bot = dr.findElement(By.xpath("//input[@type=\"submit\"]"));
+            bot.click();
+            Thread.sleep(13000);
+
+            List <WebElement> buying = dr.findElements(By.xpath(Good));
+
+            WebElement buy = buying.get(0);
+            buy.click();
+        }
+                }
+
+
     public void quitDriver(){
+
         dr.quit();
     }
 }
